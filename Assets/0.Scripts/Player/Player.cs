fd623e87f3738dbfae70374f44fcdc0de6055d25
Imperor <imperor0103@gmail.com>
2025-03-24 16:45:49 +0900

    [WIP] Cinemachine and Map asset

--- Assets/0.Scripts/Player/Player.cs
+++ Assets/0.Scripts/Player/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     /// field 안쓰고 Header, SerializeField 만 쓰면 헤더의 선언이 겹친다
     /// 따라서 각각 field: 붙인다
     /// 
-    /// PlayerAnimationData는 Serializable 선언이 있어야 한다
+    /// PlayerAnimationData는 Seria  lizable 선언이 있어야 한다
     /// SerializeField만으로는 안보여
     /// </summary>
     [field: SerializeField] public PlayerSO Data { get; private set; }  // 플레이어의 SO
@@ -34,13 +34,13 @@ public class Player : MonoBehaviour
 
         stateMachine = new PlayerStateMachine(this);
 
-        // 수정사항
-        stateMachine.ChangeState(stateMachine.IdleState);   /// 가장 처음으로 들어가는 State는 Idle
     }
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        // 수정사항
+        stateMachine.ChangeState(stateMachine.IdleState);   /// 가장 처음으로 들어가는 State는 Idle
     }
 
     // 생명주기 함수
